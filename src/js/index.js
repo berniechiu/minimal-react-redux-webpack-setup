9be4a9717a26b6af017b6a532f7d7c95e2fd2128
Bernie Chiu <bernie_chiu@hotmail.com>
2018-01-06 21:42:11 +0800

    App component adn redux store

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 import store from "../js/store/index";
-import { addArticle } from "../js/actions/index";
+import App from "../js/components/App";
 
-window.store = store;
-window.addArticle = addArticle;
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
